9edfd3c6bca4de08c595d826b438ca2e846a9a76
SMJ <muhammadj110@gmail.com>
2022-10-25 15:24:19 +0330

    added python script

---
+++ final.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+data = pd.read_excel('target.xlsx',sheet_name = 'EXAM WHOLE BOOK', usecols = 'B:H', header=None, skiprows=6)
+
+f = open('exam whole book.txt', 'w')
+# for i in range(28):
+i = 0
+while True:
+    for j in range(7):
+        if j == 0:
+            f.write(str(data.iloc[i,j])+'\n')
+        elif j == 1:
+            f.write('A. '+str(data.iloc[i,j])+'\n')
+        elif j == 2 and str(data.iloc[i,j]) != 'nan':
+            f.write('B. '+str(data.iloc[i,j])+'\n')
+        elif j == 3 and str(data.iloc[i,j]) != 'nan':
+            f.write('C. '+str(data.iloc[i,j])+'\n')
+        elif j == 4 and str(data.iloc[i,j]) != 'nan':
+            f.write('D.: '+str(data.iloc[i,j])+'\n')
+        elif j == 5 and str(data.iloc[i,j]) != 'nan':
+            f.write('E. '+str(data.iloc[i,j])+'\n')
+        elif j == 6:
+            f.write('Answer: '+str(data.iloc[i,j])+'\n\n')
+    i += 1
+    try:
+        if str(data.iloc[i,0]) == 'nan':
+            break
+    except:
+        break
+f.close
+print(data)
