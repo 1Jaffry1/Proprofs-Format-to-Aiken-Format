a1dfd028decd4a30ce2211b110af05dda2f03d08
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-01-13 02:43:07 +0330

    VERSION 2.1 RELEASED
    Major Bug fixes, Added 'Easy Mode' to run without changing the source code.

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 This is a script designed to extract data from proprofs template excel sheet, and write in Aiken Format to a .txt file. 
 
-RUN: 
- 1.  In final.py, rename 'ReadFromThis.xlsx' to the excel sheet path.
- 2.  Rename 'FromThisSheet' to the name of the sheet in the excel file that will be read by the script.
- 3.  Rename 'writeToThis.txt' to path of the file you want it to be saved to.
- 4.  Run 'final.py'
+
+RUN (Easy Mode):
+ 1.  Run 'final.py', and Enter 0.
+ 2.  Follow the prompt instructions and enter details carefully. You may use relative path if the file is contained withing the same folder as 'final.py', otherwise use absolute path. 
+
+RUN (Developer Mode): 
+ 1.  In 'final.py', set 'filepath' to the Excel worksheet path.
+ 2.  Set 'sheetName' to the name of the sheet in the excel file that will be read by the script.
+ 3.  Set 'targetPath' to path of the file you want it to be written to.
+ 4.  Run 'final.py' and Enter 1 in terminal when prompted.
+    -- You can remove the initial prompt from the code if you want to run the code faster or multiple times.
--- final.py
+++ final.py
@@ -1,20 +1,44 @@
-import numpy as np
 import pandas as pd
-import docx as dox
+status = True
+#you may run the file and then provide details during prompt
+#or edit the code below and run, then enter 1 to run without prompt
 
 #insert filepath below:
-filepath = "/Users/muhammad/Projects/Mafaheem/Level 2/Exam Level 2, Book 1.xlsx" 
+filepath = "BookPathHere, sample: /users/admin/file.xlsx  ,  or , file.xlsx" 
+
+sheetName = "SheetNameHere"
 
 #insert targetpath below:
-targetpath = "/Users/muhammad/Projects/Mafaheem/Level 2/Exam 1-16partial.txt"
+targetpath = "Path of text file to be created, sample /users/admin/output.txt or output.txt"
 
+if int(input("Enter 1 to run the script without the prompt, if you want the prompt interface, enter 0:\n")) == 0:
+    print("----------------------------------\nNOTE: Enter all paths without any preceeding or following \"'\" charachters")
+    filepath = input("----------------------------------\nEnter the path of the file you want to Extract data from (format should be xlsx):\n\t")
+    sheetName = input("----------------------------------0\nEnter the name of the sheet you want to extract data from:\n\t")
+    while True:
+        try:
+        #You can manipulate the extractor's settings in the line below:
+            data = pd.read_excel(filepath, sheet_name = sheetName, usecols = 'B:H', header=None, skiprows=6)
+            
+        except (FileNotFoundError):
+            print("----------------------------------")
+            print("ERROR: The File Path you provided was not found!")
+            filepath = input("\nEnter the path of the file you want to Extract data from (format should be xlsx):\n\t")
+            continue
 
+        except (ValueError):
+            print("----------------------------------")
+            print("ERROR: The sheet you provided was not found!")
+            sheetName = input("\nEnter the name of the sheet you want to extract data from:\n\t")
+            continue
+        
+        break
 
-data = pd.read_excel(filepath, sheet_name = f"Exam 1-16", usecols = 'B:H', header=None, skiprows=6)
+targetpath = input("----------------------------------\nEnter the path and name of the text file you want your data to be written to, \nif such a file does not exist, the script will create it:\nSample: /users/admin/myfile.txt\n\n\t")
+
 f = open(targetpath , 'w')
 i = 0
-m=1
-while m<13:
+while True:
     for j in range(7):
         if j == 0:
             f.write(str(data.iloc[i,j])+'\n')
@@ -31,13 +55,10 @@ while m<13:
         elif j == 6:
             f.write('ANSWER: '+str(data.iloc[i,j])+'\n\n')
     i += 1
-    m+=1
     try:
         if str(data.iloc[i,0]) == 'nan':
             break
     except:
         break
 f.close()
-
-
 print(f"PROCESS COMPLETE! FILE CREATED AT {targetpath}")
